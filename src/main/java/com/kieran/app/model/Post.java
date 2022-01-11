f9fb2191048cfed38e0d7d11415b7f91a8d51e45
Beavish <kieran.philip.williams@gmail.com>
2022-01-11 21:13:40 +0000

    post image mods for join

--- src/main/java/com/kieran/app/model/Post.java
+++ src/main/java/com/kieran/app/model/Post.java
@@ -1,16 +1,21 @@
 package com.kieran.app.model;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -46,5 +51,9 @@ public class Post {
 	@ManyToOne(fetch = LAZY)
 	@JoinColumn(name = "userId", referencedColumnName = "userId", insertable = false, updatable = false, nullable = true)
 	private User user;
+	
+	  @JsonManagedReference
+	  @OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="post")
+	  private List<PostImage> postImage;
 
 }
